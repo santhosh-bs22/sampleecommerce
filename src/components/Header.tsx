cb29ec4dc2e20dd2d80eb7badd8f4eda563a8db7
santhosh-bs22 <santhosh220506@gmail.com>
2025-10-18 18:05:49 +0530

    search color change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,7 @@ const Header: React.FC = () => {
               <input
                 type="text"
                 placeholder="Search products..."
-                className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+                className="w-full text-black pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
               />
             </div>
           </div>
